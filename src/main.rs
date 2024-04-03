28a2f4cdbc3cc7f6a1cbd4794c822a6ae31724fe
Yann SALMON <yannsalmon.pro@gmail.com>
2024-04-03 21:18:18 +0200

    using confy for config file + fine tuning the CLI interface

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,69 @@
 use clap::Parser;
+use colored::Colorize;
+use serde::Deserialize;
+use serde::Serialize;
+use std::i64;
+use std::path::Path;
+use std::path::PathBuf;
 
-#[derive(Parser)]
-#[command(name = "mksls")]
-#[command(version, about, long_about = None)]
+const APP_NAME: &str = "mksls";
+
+#[derive(Parser, Debug)]
+#[command(name = APP_NAME)]
+#[command(version)]
+#[command(about = "Make symlinks specified in files.")]
+#[command(long_about = "Make symlinks specified in files.
+--------
+This program makes the symlinks specified in files within DIR having the base FILE.
+A given file contains zero or more symlink specifications, where a symlink specification is a line with the following format:
+<TARGET_PATH> <SYMLINK_PATH>
+Notice the space in between.
+
+There can be multiple spaces, but there needs to be at least one.
+If a path contains a space, wrap it in double quotes.
+For example, if <TARGET_PATH> contains a space, write this instead:
+\"<TARGET_PATH>\" <SYMLINK_PATH>
+If you have a double quote in one of the paths... Change it!
+
+By default, the program is interactive.
+If no file is found where a given symlink is about to be made, the symlink will be made.
+However, if a file is found, you will be asked to choose between:
+    [s]kip : Don't create the symlink and move on to the next one.
+    [S]kip all : [s]kip for the current symlink and all further symlink conflicting with an existing file.
+    [b]ackup : Move the existing file in BACKUP_DIR, then make the current symlink.
+    [B]ackup all : [b]ackup for the current symlink and all further symlink conflicting with an existing file.
+    [o]verwrite : Overwrite the existing file with the symlink (beware data loss!)
+    [O]verwrite all : [o]verwrite for the current symlink and all further symlink conflicting with an existing file.
+However it can be made uninteractive by using one (and only) of these options:
+    --always-skip (equivalent to always selecting 's')
+    --always-backup (equivalent to always selecting 'b')
+There is no --always-overwrite for you to not regret it.
+")]
+// The path of the config file depends on `confy`, which uses `directories`.
+// To keep up to date!
+#[command(after_help = format!("{}
+You can provide other default values for the options:
+    --filename
+    --backup-dir
+    --depth
+in a TOML configuration file located at:
+    (Linux) $XDG_CONFIG_HOME/_project_path_ or .config/_project_path_ if $XDG_CONFIG_HOME is not set
+    (Mac) $HOME/Library/Application Support/_project_path_
+where _project_path_ is '{}/{}.toml'.
+
+Note:
+    - If you didn't write a config file yourself, one with the default values will automatically be written.
+    - Paths in the config file should be absolute.
+", "Configuration file:".bold().underline(), APP_NAME, APP_NAME))]
 struct Cli {
     /// The directory in which to scan for files specifying symlinks.
     #[clap(verbatim_doc_comment)]
     dir: String,
 
-    /// The name (including the extension) of the file(s) specifying symlinks to make.
+    /// The base (name + extension) of the file(s) specifying symlinks to make.
     ///
     /// By default, the name is "sls".
     /// If one is specified in the config file, it will be used instead.
-    ///
-    /// A symlink is specified on a single line, with the following syntax:
-    ///     <TARGET_PATH> <SYMLINK_PATH>
-    /// Notice the space separating them.
-    /// There can be multiple spaces, but there needs to be at least one.
-    /// If a path contains a space, wrap it in double quotes.
-    /// For example, if <TARGET_PATH> contains a space, write this instead:
-    ///     "<TARGET_PATH>" <SYMLINK_PATH>
     #[clap(verbatim_doc_comment)]
     #[arg(short, long)]
     filename: Option<String>,
@@ -28,12 +72,73 @@ struct Cli {
     ///     
     /// By default, depth is unlimited, meaning the program will search as deep as
     /// it can in the input directory.
+    /// If its value is specified in the config, it will be used instead (set to a
+    /// negative integer, say -1, to mean "unlimited").
     #[clap(verbatim_doc_comment)]
     #[arg(short, long)]
     depth: Option<String>,
+
+    /// The backup directory in which to store the backed up files during execution.
+    ///
+    /// By default, it is set to:
+    ///     (Linux) $XDG_CONFIG_HOME/mksls/backups/ or .config/mksls/backups/ if $XDG_CONFIG_HOME is not set
+    ///     (Mac) $HOME/Library/Application Support/mksls/backups/
+    #[clap(verbatim_doc_comment)]
+    #[arg(short, long)]
+    backup_dir: Option<String>,
+
+    /// Always skip the symlinks conflicting with an existing file.
+    ///
+    /// This make the program uninteractive.
+    /// Of course, it can't be combined with --always-backup.
+    #[clap(verbatim_doc_comment)]
+    #[clap(long, num_args = 0, conflicts_with = "always_backup")]
+    always_skip: Option<bool>,
+
+    /// Always backup the conflicting file before replacing it by the symlink.
+    ///
+    /// This make the program uninteractive.
+    /// Of course, it can't be combined with --always-skip.
+    #[clap(verbatim_doc_comment)]
+    #[clap(long, num_args = 0, conflicts_with = "always_skip")]
+    always_backup: Option<bool>,
 }
 
-fn main() {
+#[derive(Debug, Serialize, Deserialize)]
+struct Config {
+    filename: String,
+    backup_dir: String,
+    depth: i64,
+    always_skip: bool,
+    always_backup: bool,
+}
+
+impl ::std::default::Default for Config {
+    fn default() -> Self {
+        Self {
+            filename: String::from("sls"),
+            backup_dir: String::from(
+                confy::get_configuration_file_path(APP_NAME, APP_NAME)
+                    .unwrap()
+                    .parent()
+                    .unwrap()
+                    .join("backups/")
+                    .to_str()
+                    .unwrap(),
+            ),
+            depth: -1,
+            always_skip: false,
+            always_backup: false,
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
-    println!("filename: {:?}", cli.filename);
+    let cfg: Config = confy::load(APP_NAME, APP_NAME)?;
+
+    println!("{:?}", cli);
+    println!("{:?}", cfg);
+
+    Ok(())
 }
